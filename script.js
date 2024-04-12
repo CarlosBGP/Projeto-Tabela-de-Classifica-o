0e60d690b0e90bb2fc01ed0730f4226392d6c7ad
CarlosBGP <carloszus389@gmail.com>
2024-04-12 16:54:00 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,7 @@
+function exibirJogadoresnatela() {
+    var corpodaTabela = document.getElementById('corpodaTabela')
+    var linhadaTabela = "<tr><td>Paulo</td><td></td><td></td><td></td><td></td></tr>"
+
+    corpodaTabela.innerHTML = linhadaTabela;
+}
+exibirJogadoresnatela()
