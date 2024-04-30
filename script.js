ade4ab390b44ea0bc74629e8aeda3a2d04fcc74c
CarlosBGP <carloszus389@gmail.com>
2024-04-30 14:29:04 -0300

    Atualizaçoes-do-Codigo

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-function exibirJogadoresnatela() {
-    var corpodaTabela = document.getElementById('corpodaTabela')
-    var linhadaTabela = "<tr><td>Paulo</td><td></td><td></td><td></td><td></td></tr>"
+const lucas = {
+    nome: "Lucas",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
+}
+const carlos = {
+    nome: "Carlos",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
+}
 
-    corpodaTabela.innerHTML = linhadaTabela;
+const jogadores = [lucas,carlos]
+
+function exibirJogadoresnatela() {
+    var elemento = "";
+    for (var i=0; i < jogadores.length; i++) {
+        elemento += "<tr><td>" + jogadores[i].nome + "</td>"
+        elemento += "<td>" + jogadores[i].vitorias + "</td>"
+        elemento += "<td>" + jogadores[i].empates + "</td>"
+        elemento += "<td>" + jogadores[i].derrotas + "</td>"
+        elemento += "<td>" + jogadores[i].pontos + "</td>"
+        elemento += "<td> <button onclick='adicionarVitoria("+ i +")'>Vitorias</button></td>"
+        elemento += "<td><button onclick='adicionarEmpates("+ i +")'>Empates</button></td>"
+        elemento += "<td><button onclick='adicionarDerrotas("+ i +")'>Derrotas</button></td></tr>"
+    }
+    var corpodaTabela = document.getElementById('corpodaTabela')
+    corpodaTabela.innerHTML = elemento;
 }
 exibirJogadoresnatela()
+
+
+function adicionarVitoria(posicaoDoJogador) {
+    var jogador = jogadores[posicaoDoJogador]
+    jogador.vitorias++
+    jogador.pontos = calcularPonto(jogador)
+    exibirJogadoresnatela()
+}
+
+function adicionarEmpates(posicaoDoJogador) {
+    var jogador = jogadores[posicaoDoJogador]
+    jogador.empates++
+    jogador.pontos = calcularPonto(jogador)
+    exibirJogadoresnatela()
+}
+
+function adicionarDerrotas(posicaoDoJogador) {
+    
+}
+
+function calcularPonto(player) {
+    var pontos = player.vitorias * 3 + player.empates
+    return pontos
+}
